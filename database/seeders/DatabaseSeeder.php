80e258030ce077ea9333280a50a198777f577e27
Patrick Duncan BUENAVENTURA <Patrick.BUENAVENTURA@student.tafesa.edu.au>
2025-08-17 00:25:35 +0930

    create random Category, Supplier, and Product using factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Disable FK checks
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        // Delete old data in correct order
+        DB::table('products')->truncate();
+        DB::table('categories')->truncate();
+        DB::table('suppliers')->truncate();
+
+        // Enable FK checks
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        $this->call([
+            CategorySeeder::class,
+            SupplierSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,11 +12,11 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::post('/register', AuthController::class . 'register');
-Route::post('/login', AuthController::class . 'login');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', AuthController::class . 'logout');
+    Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::apiResource('categories', CategoryController::class);
     Route::apiResource('products', ProductController::class);
